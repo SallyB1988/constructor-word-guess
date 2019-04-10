38caa1d8af4aafdfd2566386571a4b692badded5
Sally Beckwith <sallypbeckwith@gmail.com>
2019-04-09 22:33:20 -0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var inquirer = require('inquirer');
 var Phrase = require('./Phrase');
 
+const phraseBank = [
+  "A stitch in time saves nine",
+  "a few fries short of a happy meal",
+  "life is like a box of chocolates",
+];
 
-
-const randomPhrase = "Hot dogs are good".toUpperCase();
+let counter = 0;
+let randomPhrase = phraseBank[counter].toUpperCase();
 let puzzle = new Phrase(randomPhrase);
 
 const loopPrompt = () => {
-
+  
   inquirer.prompt([
     {
       type: 'string',
@@ -23,9 +28,11 @@ const loopPrompt = () => {
       puzzle.showPhrase();
       if (puzzle.solved) {
         console.log("\n\nCongratulations! You Won!");
+        playAgain();
       } else if (puzzle.lost) {
         console.log("\n\nYou lost.");
         console.log(`The phrase was: ${randomPhrase}`);
+        playAgain();
       } else {
         loopPrompt();
       }
@@ -33,8 +40,35 @@ const loopPrompt = () => {
   });
 }
 
-console.log('Guess letters to discover the hidden phrase.');
-console.log('If you guess 10 incorrect letters, you lose.\n\n');
-console.log(randomPhrase);
-puzzle.showPhrase();
-loopPrompt();
+const playAgain = () => {
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirm',
+      message: 'Would you like to play another round? ',
+      default: true,
+    },
+  ])
+  .then(resp => {
+    if (resp.confirm) {
+      counter++;
+      if (counter < phraseBank.length) {
+        randomPhrase = phraseBank[counter].toUpperCase();
+        puzzle = new Phrase(randomPhrase);
+        puzzle.showPhrase();
+        loopPrompt();
+      } else {
+        console.log("All puzzles have been solved.")
+        return;
+      }
+    }
+  })
+}
+
+
+  
+  console.log('Guess letters to discover the hidden phrase.');
+  console.log('If you guess 10 incorrect letters, you lose.\n\n');
+  puzzle.showPhrase();
+  loopPrompt();
+
